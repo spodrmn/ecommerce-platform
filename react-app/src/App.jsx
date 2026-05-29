6cf6d469421255465a7874fa06d7d0b8f9bc87dd
seandominicyap-cpu <sean.dominic.yap@student.ateneo.edu>
2026-05-29 18:57:29 +0800

    Added product grid

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import Grid from './components/Grid'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
@@ -9,6 +11,14 @@ function App() {
         <div>
           <NavBar />
           <Hero />
+              <h1 style ={{
+                paddingTop: "50px", textAlign: "left",
+                paddingLeft: "50px",
+                paddingBottom: "30px"
+                }}> 
+                Products: 
+                </h1>
+          <Grid/>
         </div>
     </BrowserRouter>
   )
---
+++ react-app/src/components/Grid.jsx
@@ -0,0 +1,21 @@
+function Grid() {
+  return (
+<div class="container text-center">
+  <div class="row align-items-start">
+    <div class="col">
+      Product 1
+    </div>
+    <div class="col">
+      Product 2
+    </div>
+    <div class="col">
+      Product 3
+    </div>
+  </div>
+</div>
+
+
+  );
+}
+
+export default Grid;
